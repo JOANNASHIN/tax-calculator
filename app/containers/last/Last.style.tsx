89f3a9bc60c19b9a7c70aa328ece9d185d311683
joannashin <joannashin3@gmail.com>
2024-03-17 20:53:58 +0900

    last page작업

--- app/containers/last/Last.style.tsx
+++ app/containers/last/Last.style.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 const style = css`
   width: 100%;
   height: 100%;
-  background: url('/assets/images/content/main-image.png') no-repeat center center / auto 100%;
+  background: url('/assets/images/content/last-image.png') no-repeat center center / auto 100%;
 
   @media (max-width: 600px) {
     background: url('/assets/images/content/mo-main-image.png') no-repeat 55% center / auto 100%;
