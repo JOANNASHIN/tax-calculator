ef8c53d572848631c86916a27875408842a5a160
joannashin <joannashin3@gmail.com>
2024-04-06 23:58:41 +0900

    firebase setting

--- app/api/firebase.js
+++ app/api/firebase.js
@@ -1,21 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBaXGNn0DKPwnWU64KSqdenCwxM6BYsvNU",
-  authDomain: "academy-mingle.firebaseapp.com",
-  projectId: "academy-mingle",
-  storageBucket: "academy-mingle.appspot.com",
-  messagingSenderId: "131254434421",
-  appId: "1:131254434421:web:8f430c2114efeada6a563c",
-  measurementId: "G-JTGRPKFFW6"
+  apiKey: "AIzaSyBWRS7SZk-4KjGGikkc9Eidg84NHUCD6e8",
+  authDomain: "card-maker-4d938.firebaseapp.com",
+  projectId: "card-maker-4d938",
+  storageBucket: "card-maker-4d938.appspot.com",
+  messagingSenderId: "555647838979",
+  appId: "1:555647838979:web:77308399e270cda2f5d736"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import '@/styles/global.scss';
 import { RecoilRoot } from 'recoil';
+import '@/api/firebase';
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
